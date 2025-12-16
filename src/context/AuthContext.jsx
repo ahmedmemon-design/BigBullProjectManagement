a8d0362621c5aa49ded19a9937e8c8521ae5a439
ahmedmemon-design <ahmed.memon@bigbulldigital.com>
2025-12-16 13:09:47 +0500

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -57,7 +57,7 @@ const signUp = async (email, password, name) => {
     email,
     password,
     options: {
-      emailRedirectTo: "http://localhost:5173",
+      emailRedirectTo: "https://big-bull-digital.netlify.app/",
     },
   });
 
@@ -107,3 +107,4 @@ const signUp = async (email, password, name) => {
     </AuthContext.Provider>
   );
 }
+
